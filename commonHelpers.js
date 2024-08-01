04c2e5ceba96c23d0c8c7836c8e1fe7e453aa039
AiHikki <140841723+AiHikki@users.noreply.github.com>
2024-08-01 13:33:10 +0000

    Deploying to gh-pages from @ AiHikki/cookie-crunch-classic@2c0c307c62c286d50be0b93525b90ba09b1fb151 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,2 @@
-import"./assets/burger-84d5566a.js";import{S as e}from"./assets/vendor-688b6f4b.js";new e("#slider1",{perPage:4,start:1,focus:"center",autoplay:!0,rewind:!0,speed:600,interval:2e3,gap:32,trimSpace:!0,arrows:!1,pagination:!1,fixedWidth:380,fixedHeight:529,breakpoints:{1439:{perPage:3,gap:16,fixedWidth:220,fixedHeight:306,trimSpace:!1}}}).mount();
+import"./assets/burger-7b6aa160.js";import{S as e}from"./assets/vendor-688b6f4b.js";new e("#slider1",{perPage:4,start:1,focus:"center",autoplay:!0,rewind:!0,speed:600,interval:2e3,gap:32,trimSpace:!0,arrows:!1,pagination:!1,fixedWidth:380,fixedHeight:529,breakpoints:{1439:{perPage:3,gap:16,fixedWidth:220,fixedHeight:306,trimSpace:!1}}}).mount();
 //# sourceMappingURL=commonHelpers.js.map
